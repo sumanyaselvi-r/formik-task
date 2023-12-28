638f1c40fc246c1eab2a23c81802f39ffe5dd3b4
sumanyaselvi-r <145419732+sumanyaselvi-r@users.noreply.github.com>
2023-12-28 14:01:29 +0530

    Update Book.js

--- src/Book.js
+++ src/Book.js
@@ -1,4 +1,3 @@
-// BookList.js
 
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
@@ -6,15 +5,49 @@ import BookForm from './BookForm';
 
 const BookList = () => {
   const [books, setBooks] = useState([]);
-  const [editingBookId, setEditingBookId] = useState(null);
+  const [selectedBook, setSelectedBook] = useState(null);
 
   useEffect(() => {
-    // Fetch books from your API and update the state
+    // Fetch books from the mock API when the component mounts
     axios.get('https://65704af509586eff66411157.mockapi.io/books')
-      .then(response => setBooks(response.data))
-      .catch(error => console.error('Error fetching books:', error));
+      .then((response) => {
+        setBooks(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching books:', error);
+      });
   }, []);
 
+  const handleEditBook = (bookId) => {
+    setSelectedBook(bookId);
+  };
+
+  const handleDeleteBook = (bookId) => {
+    axios.delete(`https://65704af509586eff66411157.mockapi.io/books/${bookId}`)
+      .then(() => {
+        setBooks(books.filter(book => book.id !== bookId));
+      })
+      .catch((error) => {
+        console.error('Error deleting book:', error);
+      });
+  };
+
+  const handleEditClick = (editedBook) => {
+    axios.put(`https://65704af509586eff66411157.mockapi.io/books/${editedBook.id}`, editedBook)
+      .then(() => {
+        setBooks(books.map(book =>
+          book.id === editedBook.id ? editedBook : book
+        ));
+        setSelectedBook(null); // Reset selectedBook after editing
+      })
+      .catch((error) => {
+        console.error('Error updating book:', error);
+      });
+  };
+
+  const handleCancelEdit = () => {
+    setSelectedBook(null); // Reset selectedBook on cancel
+  };
   const handleAddBook = (newBook) => {
     // Add new book to the API and update the state
     axios.post('https://65704af509586eff66411157.mockapi.io/books', newBook)
@@ -22,64 +55,49 @@ const BookList = () => {
       .catch(error => console.error('Error adding book:', error));
   };
 
-  const handleEditBook = (editedBook) => {
-    // Edit book in the API and update the state
-    axios.put(`https://65704af509586eff66411157.mockapi.io/books/${editedBook.id}`, editedBook)
-      .then(() => {
-        setBooks(books.map(book => (book.id === editedBook.id ? editedBook : book)));
-        setEditingBookId(null);
-      })
-      .catch(error => console.error('Error editing book:', error));
-  };
-
-  const handleDeleteBook = (bookId) => {
-    // Delete book from the API and update the state
-    axios.delete(`https://65704af509586eff66411157.mockapi.io/books/${bookId}`)
-      .then(() => setBooks(books.filter(book => book.id !== bookId)))
-      .catch(error => console.error('Error deleting book:', error));
-  };
-
-  const handleEditClick = (bookId) => {
-    setEditingBookId(bookId);
-  };
-
-  const handleCancelEdit = () => {
-    setEditingBookId(null);
-  };
 
   return (
-  <>
+    <>
     <div className='AuthorList'>
       <h2>Book List</h2>
-      <ul style={{listStyle:'none'}}>
-        {books.map(book => (
+      <ul className="book-list" style={{listStyle:'none'}}>
+        {books.map((book) => (
           <li key={book.id} className="AuthorItem">
-            {editingBookId === book.id ? (
+            {selectedBook === book.id ? (
               <BookForm
                 initialValues={book}
-                onSubmit={handleEditBook}
+                onSubmit={handleEditClick}
                 onCancel={handleCancelEdit}
               />
             ) : (
               <>
-              <div style={{paddingLeft:'10px'}}>
-                <div>Book title:{book.title}</div>
-                <div>Book author:{book.author}</div>
-                <div>Book ISBN:{book.ISBN}</div>
-                <div>PublicationDate:{book.publicationDate}</div>
-                <button className="EditButton" onClick={() => handleEditClick(book.id)}>Edit</button>
-                <button className="DeleteButton" onClick={() => handleDeleteBook(book.id)} style={{paddingLeft:'20px',marginLeft:'20px'}}>Delete</button>
+                <div style={{ paddingLeft: '10px' }}>
+                  <div className="BookTitle">{book.title}</div>
+                  <div>Author: {book.author}</div>
+                  <div>Published: {book.publishedDate}</div>
+                  <p className="BookDescription">Description: {book.description}</p>
+                  <button
+                    className="EditButton"
+                    onClick={() => handleEditBook(book.id)}
+                  >
+                    Edit
+                  </button>
+                  <button
+                    className="DeleteButton"
+                    onClick={() => handleDeleteBook(book.id)}
+                    style={{ paddingLeft: '20px', marginLeft: '20px' }}
+                  >
+                    Delete
+                  </button>
                 </div>
               </>
-
             )}
           </li>
         ))}
       </ul>
+    </div>
     
-      
-      </div>
-      <h2>Add New Book</h2>
+<h2>Add New Book</h2>
       <BookForm initialValues={{ title: '', author: '', ISBN: '', publicationDate: '' }} onSubmit={handleAddBook} />
    
     </>
